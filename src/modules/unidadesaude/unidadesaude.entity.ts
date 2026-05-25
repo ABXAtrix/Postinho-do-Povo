1c5274ccb09ffe58514665dcd26e0c783effc14f
ABXAtrix <macedo.lac157@gmail.com>
2026-05-25 18:01:34 -0300

    Criação da classe unidadesaude e relacionamento entre usuario e unidadesaude estabelecidas

---
+++ src/modules/unidadesaude/unidadesaude.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity('unidade_saude')
+export class UnidadeSaude {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @OneToMany(() => User, (user) => user.unidade)
+  usuarios?: User[];
+
+  @Column()
+  endereco: string;
+
+  @Column()
+  bairro: string;
+
+  @Column()
+  telefone: string;
+
+  @Column()
+  tipo: string; // Ex: "USF", "UPA", "Centro de Saúde"
+
+  @Column({ name: 'horario_funcionamento' })
+  horarioFuncionamento: string; // Ex: "07:00 às 17:00"
+
+  constructor(dados?: Partial<UnidadeSaude>) {
+    this.id = dados?.id ?? 0;
+    this.nome = dados?.nome ?? '';
+    this.usuarios = dados?.usuarios ?? [];
+    this.endereco = dados?.endereco ?? '';
+    this.bairro = dados?.bairro ?? '';
+    this.telefone = dados?.telefone ?? '';
+    this.tipo = dados?.tipo ?? '';
+    this.horarioFuncionamento = dados?.horarioFuncionamento ?? '';
+  }
+}
