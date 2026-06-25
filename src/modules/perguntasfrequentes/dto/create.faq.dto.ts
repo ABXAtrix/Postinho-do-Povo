0b71be3fc278fdeee1597e2c9ccb795da440ce6e
ABXAtrix <macedo.lac157@gmail.com>
2026-06-24 23:24:59 -0300

    feat(perguntas): Mapeamento create.faq.dto.ts para a classe faq

---
+++ src/modules/perguntasfrequentes/dto/create.faq.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsString, IsInt, IsOptional } from 'class-validator';
+
+export class CreateFaqDTO {
+  @IsString({ message: 'A pergunta precisa ser uma string.' })
+  @IsNotEmpty({ message: 'A pergunta não pode ser vazia.' })
+  pergunta!: string;
+
+  @IsString({ message: 'A resposta precisa ser uma string.' })
+  @IsNotEmpty({ message: 'A resposta não pode ser vazia.' })
+  resposta!: string;
+
+  @IsInt({ message: 'O ID da campanha precisa ser um número inteiro.' })
+  @IsOptional()
+  campanhaId?: number;
+
+  @IsInt({ message: 'O ID da unidade precisa ser um número inteiro.' })
+  @IsOptional()
+  unidadeId?: number;
+}
